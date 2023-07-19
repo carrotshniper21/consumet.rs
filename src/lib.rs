4f16eca70e8a67e008f8a70378b7712cf8545850
carrotshniper21 <vipershniper21@gmail.com>
2023-07-19 22:30:28 +0000

    Update src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,3 @@ pub mod extractors;
 pub mod models;
 pub mod providers;
 pub mod utils;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
