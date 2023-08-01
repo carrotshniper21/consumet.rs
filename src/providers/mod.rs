3b3c00522ed9e899637963aecf0b51f0950ed3b4
carrotshniper21 <vipershniper21@gmail.com>
2023-08-01 18:27:46 -0500

    Create provider folders

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,3 +1,19 @@
+pub mod anime;
+pub mod books;
+pub mod comics;
+pub mod light_novels;
+pub mod manga;
+pub mod meta;
 pub mod movies;
+pub mod news;
+pub mod others;
 
+pub use anime::*;
+pub use books::*;
+pub use comics::*;
+pub use light_novels::*;
+pub use manga::*;
+pub use meta::*;
 pub use movies::*;
+pub use news::*;
+pub use others::*;
